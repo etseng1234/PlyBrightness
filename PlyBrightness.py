8265ea55644aaba1fe0a598205c2a37896d75611
etseng1234 <etseng1234@yahoo.com>
2020-07-29 23:32:02 -0700

    added contrast setting in args

--- PlyBrightness.py
+++ PlyBrightness.py
@@ -1,7 +1,9 @@
 import sys
 
-def main(argsBright, argsFile, argsSave):
+def main(argsBright, argsContrast, argsFile, argsSave):
     changeInt = argsBright
+    contrastInt = int(argsContrast)
+    contrastFactor = (259 * (contrastInt + 255)) / (255 * (259 - contrastInt))
     file = open(argsFile, 'r')
     newfile = open(argsSave, 'w')
     newfile = open(argsSave, 'a')
@@ -12,9 +14,12 @@ def main(argsBright, argsFile, argsSave):
             x = linearr[0]
             y = linearr[1]
             z = linearr[2]
-            r = checkBright(int(linearr[3]) + int(changeInt))
-            g = checkBright(int(linearr[4]) + int(changeInt))
-            b = checkBright(int(linearr[5]) + int(changeInt))
+            brightr = int(linearr[3]) + int(changeInt)
+            brightg = int(linearr[4]) + int(changeInt)
+            brightb = int(linearr[5]) + int(changeInt)
+            r = int(round(checkValid(contrastFactor * (brightr - 128) + 128)))
+            g = int(round(checkValid(contrastFactor * (brightg - 128) + 128)))
+            b = int(round(checkValid(contrastFactor * (brightb - 128) + 128)))
             op = line[6]
             newline = str(x) + ' ' + str(y) +  ' ' + str(z) + ' ' + str(r) + ' ' + str(g) + ' ' + str(b) + ' ' + '255' + ' '
             newfile.write(newline + "\n")
@@ -23,11 +28,13 @@ def main(argsBright, argsFile, argsSave):
     file.close()
     newfile.close()
 
-def checkBright(brightness):
+def checkValid(brightness):
     if(brightness > 255):
         return 255
+    elif(brightness < 0 ):
+        return 0
     else:
         return brightness
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
